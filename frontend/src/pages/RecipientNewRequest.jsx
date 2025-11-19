0e38dad41f1951ec715bbb55fe72df2a6c3f0dc8
Favgirlfr <joantirop20@gmail.com>
2025-11-20 00:32:33 +0300

    Updates

--- frontend/src/pages/RecipientNewRequest.jsx
+++ frontend/src/pages/RecipientNewRequest.jsx
@@ -13,7 +13,7 @@ export default function RecipientNewRequest() {
     const token = localStorage.getItem("token");
 
     try {
-      const res = await fetch("http://localhost:5000/api/recipient/request", {
+      const res = await fetch("${import.meta.env.VITE_API_URL}/recipient/request", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
